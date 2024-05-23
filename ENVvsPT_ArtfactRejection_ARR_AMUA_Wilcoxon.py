e5051ac1135331232f5a7b2449a46ece21157c3f
Shiyi FANG <43878064+fsy2099@users.noreply.github.com>
2024-05-23 23:39:49 +0800

    Update ENVvsPT_ArtfactRejection_ARR_AMUA_Wilcoxon.py
    .

--- ENVvsPT_ArtfactRejection_ARR_AMUA_Wilcoxon.py
+++ ENVvsPT_ArtfactRejection_ARR_AMUA_Wilcoxon.py
@@ -17,13 +17,15 @@ ori_names = ea.get_names(sig_path, "_OriginSigArray.npy")
 for ori_name in ori_names:
     ori_sig = ea.load_original(ori_name, sig_path)
     artifact_rejection = ea.ArtifactRejection(ori_sig)
-    artifact_rejection.creat_clean_array()
+    artifact_rejection.clean_array_ready()
+    artifact_rejection.ARR_array_ready()
     for cc in range(artifact_rejection.nChannel):
         for ff in range(artifact_rejection.nRate):
             for dd in range(artifact_rejection.nDur):
                 for ii in range(artifact_rejection.nITD):
                     for jj in range(artifact_rejection.nITD):
                         clean_sig = artifact_rejection.reject_artifact(cc, ff, dd, ii, jj)
+                        
                         
                         
                         
