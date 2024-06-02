22129c706902adc74fb8ad335609bfb7c289b8e4
Shiyi FANG <43878064+fsy2099@users.noreply.github.com>
2024-06-02 12:31:02 +0800

    Update ENVvsPT_ArtfactRejection_ARR_AMUA_Wilcoxon.py
    done ARR calculation
    done AMUA calculation

--- ENVvsPT_ArtfactRejection_ARR_AMUA_Wilcoxon.py
+++ ENVvsPT_ArtfactRejection_ARR_AMUA_Wilcoxon.py
@@ -4,6 +4,7 @@ Created on Tue Apr 16 15:04:54 2024
 
 @author: shiyi
 """
+from matplotlib import pyplot as plt
 import sys
 import numpy as np
 import numpy.matlib
@@ -12,6 +13,7 @@ import os
 import EphysAnalysis as ea
 #%%
 sig_path = ea.ask_directory()
+# sig_path = 'D:/0_Project/0_ENVvsPT_Ephys/Data/ND/2022_01_24'
 ori_names = ea.get_names(sig_path, "_OriginSigArray.npy")
 #%%
 for ori_name in ori_names:
@@ -19,12 +21,23 @@ for ori_name in ori_names:
     artifact_rejection = ea.ArtifactRejection(ori_sig)
     artifact_rejection.clean_array_ready()
     artifact_rejection.ARR_array_ready()
-    for cc in range(artifact_rejection.nChannel):
+    AMUA_calculation = ea.AMUACalc()
+    for cc in range(artifact_rejection.nChannel):    
         for ff in range(artifact_rejection.nRate):
             for dd in range(artifact_rejection.nDur):
                 for ii in range(artifact_rejection.nITD):
                     for jj in range(artifact_rejection.nITD):
+                        '''
+                        artifact rejection
+                        '''
                         clean_sig = artifact_rejection.reject_artifact(cc, ff, dd, ii, jj)
+                        artifact_rejection.calc_ARR(cc, ff, dd, ii, jj)
+                        artifact_rejection.calc_ARRmean(cc, ff, dd, ii, jj)
+                        '''
+                        AMUA
+                        '''
+                        AMUA_calculation.calc_AMUA(clean_sig, cc, ff, dd, ii, jj)
+                        
                         
                         
                         
